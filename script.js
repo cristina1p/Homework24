7706d03bfde106af50b2f3923be5dfc0174062b4
cristina1p <c1plamadeala@gmail.com>
2024-08-26 17:28:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,106 @@
+// Exercitii la curs
+
+// Exercitiul 1
+
+// function welcome() {
+//     console.log('Welcome everyone!')
+// }
+
+// welcome();
+
+
+
+// // Exercitiul 2
+
+// let a = 7;
+// let b = 2;
+
+// function rectangleArea() {
+//     console.log(`The area of the rectangle is ${a * b}.`)
+
+// }
+
+// rectangleArea();
+
+
+
+// // Exercitiul 3
+
+// let name = 'George';
+
+// function helloName() {
+//     console.log(`Hello, ${name}!`)
+// }
+
+// helloName(name);
+
+
+
+
+// // Exercitiul 4 
+
+// let number = 6;
+
+// function double(number) {
+//     console.log(`${number ** 2}`);
+// }
+
+// double(number);
+
+
+
+// // Exercitiul 5
+
+// let year;
+
+// function isLeapYear(year) {
+//     if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+
+// }
+
+// console.log(isLeapYear(1900));
+
+
+
+
+
+// Exercitiul echipele de sport v.2   
+
+
+// function calcAverage(a, b, c) {
+//     return (a + b + c) / 3;
+// }
+
+//console.log(calcAverage(2, 4, 7))
+
+// Test 1
+
+//let averageDolphins = calcAverage(44, 23, 71)
+//let averageKoalas = calcAverage(65, 54, 49)
+
+//console.log(averageDolphins, averageKoalas);
+
+// const checkWinner = function (averageDolphins, averageKoalas) {
+//     if (averageDolphins >= averageKoalas * 2) {
+//         return (`Dophins win (${averageDolphins} vs. ${averageKoalas}))`)
+//     } else if (averageKoalas >= averageKoalas * 2) {
+//         return (`Koalas win (${averageKoalas}) vs. ${averageDolphins} `)
+//     } else {
+//         return (`No team wins.. :(`)
+//     }
+// }
+
+//console.log(checkWinner(averageDolphins, averageKoalas))
+
+
+// Test 2
+
+// let averageDolphins = calcAverage(85, 54, 41)
+// let averageKoalas = calcAverage(23, 34, 27)
+
+// console.log(averageDolphins, averageKoalas);
+// console.log(checkWinner(averageDolphins, averageKoalas))
